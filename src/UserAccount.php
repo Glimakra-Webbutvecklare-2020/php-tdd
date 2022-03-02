d01888a39eb2b008b98c903c2a766808c63664a2
frozenbanan <henrybergstrom@protonmail.com>
2022-03-02 11:56:20 +0100

    after working with tdd

--- src/UserAccount.php
+++ src/UserAccount.php
@@ -8,7 +8,38 @@ final class UserAccount {
 
     private function __construct(string $email, string $password)
     {
+        $this->ensureIsValidEmail($email);
+        $this->ensureIsValidPassword($password);
+
         $this->email = $email;
         $this->password = $password;
+    }
+
+    public static function init(string $email, string $password) {
+        return new self($email, $password);
+    }
+
+    private function ensureIsValidEmail(string $email): void
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+
+    private function ensureIsValidPassword(string $password): void
+    {
+        if (strlen($password) <= 8 || preg_match("#^[a-zA-Z0-9]+$#", $password)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid password length, or needs numbers, or needs special characters',
+                    $password
+                )
+            );
+        }
     }
 }
--- tests/UserAccountTest.php
+++ tests/UserAccountTest.php
@@ -5,21 +5,45 @@ use Henry\PhpUnitTestExample\UserAccount;
 
 
 // Requirements:
-// An user account can be created from a valid email string and password
-// An user account can NOT be created from a invalid email string
-// A user account password should be over 8 characters long and have letters, numbers and special characters
+// An user account can be created from a valid email string and password [OK]
+// An user account can NOT be created from a invalid email string [OK]
+// A user account password should be over 8 characters long [OK]
+// A user account password should include letters, numbers and special characters
 // A valid user account should be able to login
 // A logged in user should be able to log out
 
 
 class UserAccountTest extends TestCase
-{
+{   
+    // private $invalidPasswordByLength = "pd1!@";
+    // private $invalidPasswordByLetter = "11111111111111";
+    // private $invalidPasswordByNumber = "password$$";
+    // private $invalidPasswordBySpecialChar = "password1123";
+
     public function testCanBeCreatedFromValidEmailAddressAndPassword(): void
     {
         $this->assertInstanceOf(
             UserAccount::class,
-            UserAccount::fromString('user@example.com', "passw0rd")
+            UserAccount::init('user@example.com', "passw0rd123123!@")
         );
+    }
+
+    public function testCanNotBeCreatedFromInvalidEmailString(): void {
+        $this->expectException(InvalidArgumentException::class);
+
+        UserAccount::init('invalid', "passw0rd123123!@");
+    }
+
+    public function testUserPasswordMustBeOver8Characters(): void {
+        $this->expectException(InvalidArgumentException::class);
+
+        UserAccount::init('user@example.com', "2short@");
+    }
+
+    public function testUserPasswordMustIncludeLettersNumberAndSpecialCharacters(): void {
+        $this->expectException(InvalidArgumentException::class);
+        UserAccount::init('user@example.com', "longpasswordwithoutnumbers");
+
     }
 }
 
